334e318be8b86ad1a920285667d3b96457766055
scn vincent <145318293+scnscnscn@users.noreply.github.com>
2024-12-03 22:07:14 +0800

    instruction

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+setup(
+    name='Chatbot',
+    version='0.1.0',
+    author='Vincent Van',
+    author_email='WLQVincent@outlook.com',
+    description='Chatbot,weibo crawler,nlpmode training',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/scnscnscn/ChatBot',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'python-docx',
+        'transformers',
+        'torch',
+        'tensorflow',
+        'opencv-python',
+        'redis',
+        'pandas',
+        'matplotlib',
+        'scikit-learn',
+        'datasets'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.8',
+    keywords='Chatbot',
+)
